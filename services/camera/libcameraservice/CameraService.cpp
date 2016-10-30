a1c83157ab3e3cff9b0380e22836bece54e9134a
Simonas Leleiva <simonas.leleiva@meramo.co.uk>
2016-10-30 14:07:11 +0000

    [mer-hybris] AudioService is disabled in hybris adaptations
    Without this patch, camera is waiting for AudioService indefinitely
    
    Signed-off-by: Simonas Leleiva <simonas.leleiva@jollamobile.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -1322,9 +1322,10 @@ void CameraService::loadSound() {
     Mutex::Autolock lock(mSoundLock);
     LOG1("CameraService::loadSound ref=%d", mSoundRef);
     if (mSoundRef++) return;
-
+/*
     mSoundPlayer[SOUND_SHUTTER] = newMediaPlayer("/system/media/audio/ui/camera_click.ogg");
     mSoundPlayer[SOUND_RECORDING] = newMediaPlayer("/system/media/audio/ui/VideoRecord.ogg");
+*/
 }
 
 void CameraService::releaseSound() {
